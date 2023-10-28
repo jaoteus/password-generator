3d8049460560797917f798daab1b6284649cb099
Mateus <mateusff2323@gmail.com>
2023-10-28 18:22:13 -0300

    adicionado a função para gerar sennha

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 root = Tk()
 
@@ -8,20 +9,79 @@ class Aplication():
         self.tela()
         self.frames()
         self.botoes()
+        self.labels()
         root.mainloop()
     def tela(self):
         self.root.title("Gerador de Senha")
-        self.root.configure(background='gray')
+        self.root.configure(
+            background='gray'
+            )
         self.root.geometry('380x250')
         self.root.resizable(False, False) # Não irá permitir expandir a tela
     def frames(self):
-        self.frame_1 = Frame(self.root, bd=4, bg='white')
-        self.frame_1.place(relx=0.1, rely=0.4, relheight=0.2, relwidth=0.8)
+        self.frame_1 = Frame(self.root, 
+                             bd=4, 
+                             bg='white'
+                             )
+        self.frame_1.place(
+                        relx=0.05, 
+                        rely=0.4, 
+                        relheight=0.52, 
+                        relwidth=0.9
+                        )
     def botoes(self):
-        self.botaoGerar = Button(text='Gerar')
-        self.botaoGerar.place(relx=0.45, rely=0.1, relheight=0.1, relwidth= 0.1)
+        self.botao_gerar = Button(
+            text='Gerar senha',
+            command= self.gerar_senha()
+            )
+        self.botao_gerar.place(
+            relx=0.42, 
+            rely=0.1, 
+            relheight=0.1, 
+            relwidth= 0.2
+            )
+    def labels(self):
+        self.lb_senha_gerada = Label(
+                                    text='Senha gerada:',
+                                    bg='gray',
+                                    fg='white'
+                                    )
+        self.lb_senha_gerada.place(
+            relx=0.05,
+            rely=0.3, 
+            relheight=0.07, 
+            relwidth=0.2
+            )
+        
+
+
+
+
+
+
 
     def gerar_senha(self):
-        pass
+        self.letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        self.simbolos = ['!', '@', '#', '$', '%', '&']
+        self.quantidade_de_algarismo = 26
+        self.senha_gerada = ''
+        while len(self.senha_gerada) < self.quantidade_de_algarismo:
+            '''
+            1 - Alfabeto
+            2 - Número
+            3 - Símbolo
+            '''
+            self.qual_o_proximo = random.randrange(1, 4)
+            if self.qual_o_proximo == 1:
+                self.algarismo_aleatorio = random.choice(self.letras)
+                self.senha_gerada = self.senha_gerada + self.algarismo_aleatorio
+            elif self.qual_o_proximo == 2:
+                self.algarismo_aleatorio = random.choice(self.numeros)
+                self.senha_gerada = self.senha_gerada + self.algarismo_aleatorio
+            elif self.qual_o_proximo == 3:
+                self.algarismo_aleatorio = random.choice(self.simbolos)
+                self.senha_gerada = self.senha_gerada + self.algarismo_aleatorio
+            return self.senha_gerada
 
 Aplication()
