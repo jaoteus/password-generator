fa4a7763318711bd7849cec480abd435e8cd8d3b
Mateus <mateusff2323@gmail.com>
2023-10-29 19:19:18 -0300

    finalizado a primeira versão

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ class Aplication():
     def __init__(self):
         self.root = root
         self.tela()
-        # self.frames()
         self.botoes()
         self.labels()
-        # self.text_box()
-        # self.entrys()
         root.mainloop()
     def tela(self):
         self.root.title("Gerador de Senha")
@@ -19,25 +16,18 @@ class Aplication():
         self.root.geometry('380x250')
         self.root.resizable(False, False) # Não irá permitir expandir a tela
 
-    # def frames(self):
-    #     self.frame_1 = Frame(self.root, bd=4, bg='white')
-    #     self.frame_1.place(relx=0.05, rely=0.4, relheight=0.52, relwidth=0.9)
-
     def botoes(self):
-        self.botao_gerar = Button(text='Gerar senha', command=self.gerar_senha) ##### RESOLVIDO
+        self.botao_gerar = Button(text='Gerar senha', command=self.gerar_senha)
         self.botao_gerar.place(relx=0.42, rely=0.1, relheight=0.1, relwidth= 0.2)
 
     def labels(self):
-        self.msg = ''
-        self.lb_senha_gerada = Label(text='Senha gerada:',bg='gray',fg='white')
+        self.mensagem = ''' teste '''
+        self.lb_senha_gerada = Label(self.root, text='Senha gerada:',bg='gray',fg='white')
         self.lb_senha_gerada.place(relx=0.41,rely=0.3, relheight=0.07, relwidth=0.2)
-        self.lb_texto_da_senha = Text(self.root, width=10, height=10)
-        self.lb_texto_da_senha.pack(expand=True)
-        self.lb_texto_da_senha.insert('end', self.msg)
-        self.lb_texto_da_senha.config(state='disabled')
-        self.lb_texto_da_senha.place(relx=0.1, rely=0.5, relheight=0.1, relwidth=0.1)
 
-        
+        self.output_text = Text(self.root, height=5, width=30, state='normal')
+        self.output_text.pack(padx=5,pady=100)
+
     def gerar_senha(self):
         self.letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
@@ -60,7 +50,9 @@ class Aplication():
             elif self.qual_o_proximo == 3:
                 self.algarismo_aleatorio = random.choice(self.simbolos)
                 self.senha_gerada = self.senha_gerada + self.algarismo_aleatorio
-                print(self.senha_gerada)
+
+            self.output_text.delete(1.0, END)
+            self.output_text.insert(END, self.senha_gerada)
         
         
 Aplication()
